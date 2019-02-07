95051490bb958186d8ad99e52344c212efdc3ef6
Evgen Rusakov <revgen@users.noreply.github.com>
2019-02-07 13:47:12 -0500

    Update profile.ps1

--- setup/windows/userprofile/profile.ps1
+++ setup/windows/userprofile/profile.ps1
@@ -25,7 +25,7 @@ function openExplorer($path) {
     explorer "${path}"
 }
 function runVimDiff($file1, $file2) {
-    gvim -c \":set columns=200 lines=50\" -d "${file1}" "${file2}"
+    gvim -c ":set columns=200 lines=50" -d "${file1}" "${file2}"
 }
 function runExit() { [Environment]::Exit(0) }
 function cdToWorkspace() { Set-Location $WS }

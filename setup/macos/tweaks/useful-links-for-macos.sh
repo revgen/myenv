05bce0407c2e84002a5e9910ff4b732c5b1aab51
Evgen Rusakov <revgen@users.noreply.github.com>
2019-04-09 12:38:43 -0400

    Delete useful-links-for-macos.sh

--- setup/macos/tweaks/useful-links-for-macos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Resources where you can find an information about MacOS settings and tweaks"
-echo "---------------------------------------------------------------------------"
-echo "* https://www.defaults-write.com"
-echo "* https://github.com/cfibmers/dotfiles/blob/master/osx/defaults.sh"
-echo ""

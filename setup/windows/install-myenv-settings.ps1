50a508345bf868fe904b327501d922d5005131a7
Evgen Rusakov <revgen@users.noreply.github.com>
2019-09-16 09:41:11 -0400

    Update install-myenv-settings.ps1

--- setup/windows/install-myenv-settings.ps1
+++ setup/windows/install-myenv-settings.ps1
@@ -1,6 +1,6 @@
 # ==================================================================================
 # To download it with PowerShell use a command:
-# wget https://raw.githubusercontent.com/revgen/myenv/master/setup/windows/install-myenv-settings.ps1 -OutFile install-myenv-settings.ps1
+# Invoke-WebRequest https://raw.githubusercontent.com/revgen/myenv/master/setup/windows/install-myenv-settings.ps1 -OutFile install-myenv-settings.ps1
 # ==================================================================================
 
 $ErrorActionPreference = "Stop"
@@ -8,21 +8,28 @@ $ErrorActionPreference = "Stop"
 $repo = "https://raw.githubusercontent.com/revgen/myenv/master"
 $dest = $env:USERPROFILE
 
+function createDirectory($dirPath) {
+    Write-Host "Create directory ${dirPath}"
+    New-Item -ItemType Directory -Force -Path "${dirPath}" | Out-Null
+}
+
 function download($url, $output) {
     Write-Host "Downloading  file '${url}'..."
     Write-Host "  Output to ${output}"
     $dir = [System.IO.Path]::GetDirectoryName("${output}")
-    New-Item -ItemType Directory -Force -Path "${dir}" | Out-Null
-    wget "${url}" -OutFile "${output}"
+    createDirectory "${dir}"
+    Invoke-WebRequest "${url}" -OutFile "${output}"
     Write-Host "  Done, ${output} saved"
 }
 
 Write-Host "Download all settings and script into the '${dest}' directory..."
 
-download "${repo}/setup/windows/userprofile/profile.ps1" "${dest}\Documents\WindowsPowerShell\profile.ps1"
+download "${repo}/setup/windows/userprofile/Documents/WindowsPowerShell/profile.ps1" "${dest}\Documents\WindowsPowerShell\profile.ps1"
 download "${repo}/setup/windows/userprofile/.bash_profile" "${dest}\.bash_profile"
 download "${repo}/setup/windows/userprofile/.bashrc" "${dest}\.bashrc"
 download "${repo}/setup/windows/userprofile/.minttyrc" "${dest}\.minttyrc"
+createDirectory "${dest}/AppData/Roaming/bin"
+createDirectory "${dest}/Workspace"
 
 download "${repo}/home/.gitconfig" "${dest}\.gitconfig"
 download "${repo}/home/.vimrc" "${dest}\.vimrc"

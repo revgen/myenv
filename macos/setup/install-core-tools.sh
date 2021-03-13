0d08ce7814e37f241e9f652d473b8409f90443dc
Evgen <revgen@users.noreply.github.com>
2021-03-13 10:40:34 -0500

    Add myenv source code (beta)

---
+++ macos/setup/install-core-tools.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "Core toools"
+brew install moreutil
+
+
+echo "Instal core X Window System tools"
+brew install xquartz
+
+
+
+echo "Install mplayer,mpv,ffmpeg"
+brew install mplayer mpv libcaca
+
+

bb93026cffb39d10f3d345ae31ef5256c57dbdef
Evgen Rusakov <revgen@users.noreply.github.com>
2021-10-17 08:46:11 -0400

    Update install-core-tools-X.sh

--- linux/setup/ubuntu/install-core-tools-X.sh
+++ linux/setup/ubuntu/install-core-tools-X.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y xdotool wmctrl xclip
-sudo apt install -y mplayer ffmpeg webp imagemagick youtube-dl kid3 pinta
+sudo apt install -y mpv ffmpeg webp imagemagick youtube-dl kid3 pinta
 sudo apt install remmina gparted
 # sudo apt-get install -y mtpaint
 # sudo apt-get install -y mypaint   - good for painter
@@ -10,14 +10,14 @@ sudo apt install remmina gparted
 # sudo apt-get install -y encfs
 
 
-sudo apt install vim-gnome
+sudo apt install gedit
 sudo apt install keepassxc
 sudo apt install simple-scan
 sudo apt install -y lightdm lightdm-gtk-greeter-settings
 sudo apt install blueman
 sudo apt install libreoffice-calc libreoffice-writer
 
-if [ -n "$(grep "^NAME" /etc/os-release | grep -i "ubuntu")" ]; then
+if [ -n "$(grep "^NAME" /etc/os-release | grep -i "buntu")" ]; then
     # ubuntu specific tools
     sudo apt install -y dconf-tools
     sudo apt install -y gnome-sound-recorder
@@ -26,6 +26,7 @@ if [ -n "$(grep "^NAME" /etc/os-release | grep -i "ubuntu")" ]; then
 
     sudo apt purge -y gnome-software
     sudo apt purge -y pluma
+    sudo apt purge -y mousepad
     sudo apt purge -y xchat
     sudo apt purge -y brasero
     sudo apt purge gdm3

1e17018f7a2a4aedab6f6a84fbc36e5072b3842a
Evgen Rusakov <revgen@users.noreply.github.com>
2024-05-14 17:22:18 -0400

    Update install-core-tools.sh

--- setup/macos/install-core-tools.sh
+++ setup/macos/install-core-tools.sh
@@ -17,23 +17,30 @@ brew install bash bash-completion
 # glow              - see MarkDown files in terminal
 # bat               - a 'cat' tool with syntax highlight
 # lnav              - ncurses-based log file viewer
-brew install coreutils moreutil inetutils screen tree htop mc watch \
-    gettext wget telnet jq unzip p7zip lnav md5sha1sum ncdu pv \
-    bat glow shellcheck tig unrar figlet neofetch trash-cli
+brew install coreutils moreutil inetutils screen tree htop mc watch
+brew install gettext wget telnet jq unzip p7zip lnav md5sha1sum ncdu pv
+brew install bat glow shellcheck tig unrar figlet neofetch trash-cli
+# --------------------------------------------------------------------------------
 brew install httping awscli
-brew install cmatrix xscreensaver
 brew install lynx elinks openssl speedtest-cli 
 brew install ghostscript imagemagick
 brew install xquartz
 brew install git tig neovim
 brew install qview itsycal paintbrush
+brew install itsycal
 # brew install free-ruler
 brew install mpg123 ffmpeg mpv libcaca yt-dlp dmidecode kid3
-brew install vlc
-# brew install skype zoom
-brew install google-chrome google-backup-and-sync itsycal
+# brew install vlc
+# brew install skype
+# brew install zoom
+# brew install google-chrome google-backup-and-sync
 brew install iterm2 vnc-viewer kid3 balenaetcher handbrake sigil keepassxc
 brew install microsoft-remote-desktop
 
-pip3 install --user termsaver requests python-dotenv
+pip3 install --user requests python-dotenv
 pip3 install visidata
+
+# --------------------------------------------------------------------------------
+brew install cmatrix
+brew install xscreensaver
+pip3 install --user termsaver

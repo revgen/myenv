cbfbf5db8187e20af552264a4f8757a3e8bb0f03
Evgen Rusakov <revgen@users.noreply.github.com>
2024-10-08 21:29:23 -0400

    Update install-brew-package-manager.sh

--- setup/macos/install-brew-package-manager.sh
+++ setup/macos/install-brew-package-manager.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
-echo "==[ Install command line tool ]========================================="
-xcode-select --install
-
-if [ -n "$(which xcodebuild)" ]; then
-    echo "==[ Agree to the XCode license ]========================================"
-    sudo xcodebuild -license
-fi
+# The last installation command can be found on the official page:
+# https://brew.sh/
 
 echo "==[ Install homebrew ]=================================================="
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 echo "==[ Update homebrew ]==================================================="
 brew update
 
-# echo "==[ Install Mac App Store command-line interface ]======================"
-# brew install mas
-
+# xcode-select --install
+# sudo xcodebuild -license

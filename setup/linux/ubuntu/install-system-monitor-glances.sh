210d1e5b64f23346499cd1ae6599423fdc1739f3
Evgen Rusakov <revgen@users.noreply.github.com>
2025-12-27 18:40:53 -0500

    Fix installation script for Glances service
    Updated installation script for Glances to ensure proper dependency installation and service setup.

--- setup/linux/ubuntu/install-system-monitor-glances.sh
+++ setup/linux/ubuntu/install-system-monitor-glances.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "--[Begin: $(basename "${0}")]-----------------"
+echo "Install dependencies..."
+sudo python3 -m pip install uvicorn bottle fastapi ujson --break-system-packages || exit 1
+
 echo "Install glaces to the system"
-sudo pip3 install bottle glances && \
-echo "Create glances.service file" && \
+sudo python3 -m pip install glances --break-system-packages || exit 1
+
+/usr/local/bin/glances --version
+echo "Create glances.service file"
 echo -e "[Unit]
 Description=Glances
 After=network.target
@@ -13,17 +18,23 @@ Restart=on-abort
 
 [Install]
 WantedBy=multi-user.target
-" > /tmp/glances.service && \
-echo "Put it to the system -> /etc/systemd/system/glances.service" && \
-sudo mv /tmp/glances.service /etc/systemd/system/glances.service && \
-echo "Enable unit for automatic start while booting..." && \
-sudo systemctl enable glances.service && \
-echo "Start the service" && \
+" > /tmp/glances.service
+echo "Put it to the system -> /etc/systemd/system/glances.service"
+sudo mv /tmp/glances.service /etc/systemd/system/glances.service || exit 1
+echo "Enable unit for automatic start while booting..."
+sudo systemctl enable glances.service
+echo "Start the service"
 sudo systemctl start glances.service
 
-ver=$(/usr/local/bin/glances --version | grep "v[0-9]" | head -n 1 | cut -d" " -f2 | cut -d"." -f1 | sed 's/v//g')
+echo "Installation is done."
+systemctl status glances
+
+api_ver=$(/usr/local/bin/glances --version | grep "API" | cut -d":" -f2 | sed 's/[\t ]*//g')
 echo "---------------------"
 echo "Default glaces address http://localhost:61208"
-echo "To check api status: wget -O- http://localhost:61208/api/${ver}/config"
+echo "Glances has API version = ${api_version}"
+echo "To check api status: wget -O- http://localhost:61208/api/${api_ver}/status"
+wget -qO- http://localhost:61208/api/${api_ver}/status
 echo ""
 echo "--[End: $(basename "${0}")  ]-----------------"
+
